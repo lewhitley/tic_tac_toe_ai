012bc9bfe20ce6471f000ae4db358eb0c462e0cc
Lindsey <lewhitley@gmail.com>
2016-09-20 09:21:50 -0700

    implement SuperComputer classfor tic tac toe that can't be beaten

---
+++ lib/super_computer_player.rb
@@ -0,0 +1,21 @@
+require_relative 'tic_tac_toe_node'
+
+class SuperComputerPlayer < ComputerPlayer
+  def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    children = node.children
+
+    winner = children.find { |child| child.winning_node?(mark) }
+    return winner.prev_move_pos unless winner.nil?
+
+    children.find { |child| !child.losing_node?(mark) }.prev_move_pos
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Play the brilliant computer!"
+  hp = HumanPlayer.new("Borky")
+  cp = SuperComputerPlayer.new
+
+  TicTacToe.new(hp, cp).run
+end
